f60bdf4299e5790d4c10a086f8e784acf19f4389
chenfeng <703390877@qq.com>
2018-01-22 20:48:33 +0800

    更新

--- app/dist/js/login-state.js
+++ app/dist/js/login-state.js
@@ -1,27 +1,20 @@
 $(function() {
     if (sessionStorage.token && sessionStorage.name) {
-        $('.web-nav .login').html('<a data-href="javascript:;" style="font-size: 12px;" data-i18n="nav.outLogin">退出账户</a>');
-        $('.moblie-nav').html('<a href="javascript:;" style="font-size: 12px;" data-i18n="nav.outLogin">退出账户</a>');
-
-
-
-        $('.web-nav .login a,.moblie-nav a').click(function() {
+        $('.web-nav .login').html('<a href="javascript:;" style="font-size: 12px;" data-i18n="nav.outLogin">退出账户</a>');
+        $('.web-nav .login a').click(function() {
             sessionStorage.removeItem('token');
             sessionStorage.removeItem('name');
-            var txt = $('#slide_lang dt').text();
-            if (txt == "简体中文") {
-
-                window.location.href = "index.html#cn";
-            } else {
-                window.location.href = "index.html#en";
-            }
-
+            location.href = 'index.html';
+        })
+        $('.moblie-nav').html('<a href="javascript:;" style="font-size: 12px;" data-i18n="nav.outLogin">退出账户</a>');
+        $('.moblie-nav a').click(function() {
+            sessionStorage.removeItem('token');
+            sessionStorage.removeItem('name');
+            location.href = 'index.html';
         })
 
-
-
     } else {
-        $('.web-nav .login').html('<a data-href="login.html" style="font-size: 12px;" data-i18n="nav.login">登录&nbsp;/&nbsp;注册</a>');
-        $('.moblie-nav').html('<a data-href="login.html" style="font-size: 12px;"  data-i18n="nav.login">登录 / 注册</a>');
+        $('.web-nav .login').html('<a href="login.html" style="font-size: 12px;" data-i18n="nav.login">登录&nbsp;/&nbsp;注册</a>');
+        $('.moblie-nav').html('<a href="login.html" style="font-size: 12px;"  data-i18n="nav.login">登录 / 注册</a>');
     }
 })
