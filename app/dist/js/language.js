8968d1758d47ce5e82044bc21297d02044890830
chenfeng <703390877@qq.com>
2018-01-22 11:05:08 +0800

    更新

---
+++ app/dist/js/language.js
@@ -0,0 +1,23 @@
+$(function() {
+
+
+    // i18n.init({
+    //     resGetPath: './dist/language/en/__ns__.json'
+    // }, function(t) {
+    //     // i18n est maintenant initialisé
+    //     $('body').i18n();
+
+    // });
+    // console.log(2)
+    // $('#en').click(function() {
+    //     i18n.init({
+    //         resGetPath: './dist/language/en/__ns__.json'
+
+    //     }, function(t) {
+    //         // i18n est maintenant initialisé
+    //         $('body').i18n();
+    //     });
+    // });
+    // console.log(1)
+
+})
