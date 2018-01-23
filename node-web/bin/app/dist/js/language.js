27a3aab1c87e02702e353d0b52747333d6df1f49
lsy <13295927826@163.com>
2018-01-23 16:04:59 +0800

    1

--- node-web/bin/app/dist/js/language.js
+++ node-web/bin/app/dist/js/language.js
@@ -1,10 +1,11 @@
 $(function() {
-    //导航栏下划线，切换语言，需重新调整
-    var navActive = document.querySelector(".nav-active");
-    var navUnderline = document.querySelector('.nav ul');
 
     function mouseleaveFunc() {
-        navActive = document.querySelector(".nav-active");
+        //导航栏下划线，切换语言，需重新调整
+        var navActive = document.querySelector(".nav-active");
+        var navUnderline = document.querySelector('.nav ul');
+
+
         var width = navActive.getBoundingClientRect().width;
         var height = navActive.getBoundingClientRect().height;
         var left = navActive.getBoundingClientRect().left + window.pageXOffset;
@@ -39,6 +40,7 @@ $(function() {
     $('#lan_exchange').click(function() {
 
         var txt = $(this).text().trim()
+        console.log(txt)
         if (txt == "简体中文") {
             en();
         } else if (txt == "English") {
@@ -67,6 +69,7 @@ $(function() {
 
     // 导航跳到首页页面
     for (var i = 0; i < 3; i++) {
+        // web
         $('.web-nav li').eq(i).click(function(e) {
             var e = e || window.event;
             var href = e.target.dataset.href
@@ -81,6 +84,21 @@ $(function() {
                 window.location.href = href
             }
 
+        })
+
+        //moblie
+        $('#page-list li').eq(i).find('a').click(function() {
+            var href = $(this).attr("data-href");
+            var txt = $('#slide_lang dt').text().trim()
+            if (href == undefined) { return; }
+            if (txt == "简体中文") {
+                window.location.href = href + "?cn";
+            } else if (txt == "English") {
+                window.location.href = href + "?en";
+
+            } else {
+                window.location.href = href
+            }
         })
     }
 
@@ -110,7 +128,32 @@ $(function() {
             $('body').i18n();
             window.location.hash = '#cn'
 
+            //首页section1
+            $('.section1 .picasso-txt span').css('display', 'none')
+            $('.section1 .picasso-txt img').css('display', 'inline-block')
 
+            //首页section2
+            $('.section2 .lab-text').removeClass('en-s2-p')
+
+            // 首页 优势 section3-4
+            $('.advantage .title p').removeClass('en-s3-bigTitle')
+            $('.advantage .title img').removeClass('en-s3-img')
+            $('.advantage .content-title span').removeClass('en-s3-title')
+            $('.advantage .content-title img').removeClass('en-s3-img')
+            $('.advantage .box-content .content-txt').removeClass('en-s3-txt')
+
+
+            // 首页 下载 section5
+            $('.download .box-2-title .down-app').removeClass('en-s5-app')
+            $('.download .box-2-title img').removeClass('en-s5-img')
+            $('.download .box-2-title .titile-2 span').removeClass('en-s5-txt')
+
+            // 资产管理页面, 
+            $('.assets-title img').removeClass('en-as-img')
+            $('.assets-title p').removeClass('en-as-title')
+            $('#totalsData .content-btn button').removeClass('en-as-btn')
+
+            // 首页导航下划线，长度定位调整
             if (window.location.href.indexOf('index.html') != -1) {
                 mouseleaveFunc()
             }
@@ -127,7 +170,32 @@ $(function() {
             $('body').i18n();
             window.location.hash = '#en'
 
+            //首页section1
+            $('.section1 .picasso-txt span').css('display', 'block')
+            $('.section1 .picasso-txt img').css('display', 'none')
 
+            //首页section2
+            $('.section2 .lab-text').addClass('en-s2-p')
+
+            // 首页 优势 section3-4
+            $('.advantage .title p').addClass('en-s3-bigTitle')
+            $('.advantage .title img').addClass('en-s3-img')
+            $('.advantage .content-title span').addClass('en-s3-title')
+            $('.advantage .content-title img').addClass('en-s3-img')
+            $('.advantage .box-content .content-txt').addClass('en-s3-txt')
+
+            // 首页 下载 section5
+            $('.download .box-2-title .down-app').addClass('en-s5-app')
+            $('.download .box-2-title img').addClass('en-s5-img')
+            $('.download .box-2-title .titile-2 span').addClass('en-s5-txt')
+
+
+            // 资产管理页面,  操作那块的按钮样式和弹出框layer的样式，类名直接加在JS那生成的部分
+            $('.assets-title img').addClass('en-as-img')
+            $('.assets-title p').addClass('en-as-title')
+            $('#totalsData .content-btn button').addClass('en-as-btn')
+
+            // 首页导航下划线，长度定位调整
             if (window.location.href.indexOf('index.html') != -1) {
                 mouseleaveFunc()
             }
