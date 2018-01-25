8d34e151042d3f716dda189299e90f13e04a75f1
lsy <13295927826@163.com>
2018-01-25 10:06:06 +0800

    web导航栏，语言移入移出样式

--- node-web/bin/app/dist/js/main.js
+++ node-web/bin/app/dist/js/main.js
@@ -11,8 +11,16 @@ $(function() {
             $('.navbar').hide()
         }, 500)
     })
-    $('#slide_lang').click(function() {
-        $('#slide_lang_box').toggle()
+    $('#slide_lang').mouseenter(function() {
+        // $('#slide_lang_box').toggle()
+        $('#slide_lang_box').css('display', "block")
+    })
+    $('.nav,#slide_lang_box').mouseleave(function() {
+
+        $('#slide_lang_box').css('display', "none")
+    })
+    $('#slide_lang_box').click(function() {
+        $('#slide_lang_box').css('display', "none")
     })
 })
 
